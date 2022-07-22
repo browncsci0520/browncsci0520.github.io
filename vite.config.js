3914ef5c1e8aa0e357726db7728c4e0ace2f8084
taurreco <tomasdougan@gmail.com>
2022-07-22 17:17:57 -0400

    vite build config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'homogeneous-coords-handout.html')
+        nested: resolve(__dirname, 'homogeneous-coords-handout.html'),
+        demo1: resolve(__dirname, 'pages/affine-transform-demo.html'),
+        demo3: resolve(__dirname, 'pages/projective-geometry-demo.html')
       }
     }
   }
