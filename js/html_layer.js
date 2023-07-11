9a1107197b2e85abde2750bce51d056aa45ffbf6
Julia Fu <wanjia_fu@brown.edu>
2023-07-11 16:39:26 -0400

    sidebar

--- js/html_layer.js
+++ js/html_layer.js
@@ -36,7 +36,7 @@ $('#affine-transform-demo-input-start').html($input.html());
 affineAnimateButton.onclick = function() {
     AFFINE.animate();
     let $input = $('#affine-transform-demo-input');
-    $('#affine-transform-demo-input-animate').html($input.html());
+    $('#affine-transform-demo-input-animate').html($input);
 };
 
 /* third state */
@@ -44,7 +44,7 @@ affineAnimateButton.onclick = function() {
 affineFullTransformButton.onclick = function() {
     AFFINE.fullTransform();
     let $input = $('#affine-transform-demo-input');
-    $('#affine-transform-demo-input-full-transform').html($input.html());
+    $('#affine-transform-demo-input-full-transform').html($input);
 };
 
 /* fourth state */
@@ -53,42 +53,42 @@ projStartButton.onclick = function() {
     AFFINE.reset();
     $('#affine-transform-demo-hide').html($('#affine-transform-demo-input'));
     let $input = $('#projective-geometry-demo-input');
-    $('#projective-geometry-demo-input-start').html($input.html());
+    $('#projective-geometry-demo-input-start').html($input);
 }
 
 /* fifth state */
 projSubspaceButton.onclick = function() {
     PROJ.subspaceState();
     let $input = $('#projective-geometry-demo-input');
-    $('#projective-geometry-demo-input-subspace').html($input.html());
+    $('#projective-geometry-demo-input-subspace').html($input);
 }
 
 /* sixth state */
 projLineButton.onclick = function() {
     PROJ.lineState();
     let $input = $('#projective-geometry-demo-input');
-    $('#projective-geometry-demo-input-line').html($input.html());
+    $('#projective-geometry-demo-input-line').html($input);
 }
 
 /* seventh state */
 projPlaneButton.onclick = function() {
     PROJ.startPlane();
     let $input = $('#projective-geometry-demo-input');
-    $('#projective-geometry-demo-input-plane').html($input.html());
+    $('#projective-geometry-demo-input-plane').html($input);
 }
 
 /* eighth state */
 projFrameButton.onclick = function() {
     PROJ.startFrame();
     let $input = $('#projective-geometry-demo-input');
-    $('#projective-geometry-demo-input-frame').html($input.html());
+    $('#projective-geometry-demo-input-frame').html($input);
 }
 
 /* ninth state */
 projTransButton.onclick = function() {
     PROJ.startTranslation();
     let $input = $('#projective-geometry-demo-input');
-    $('#projective-geometry-demo-input-translate').html($input.html());
+    $('#projective-geometry-demo-input-translate').html($input);
 }
 
 /*
@@ -107,12 +107,12 @@ persStartButton.onclick = function() {
     PERS.start();
     $('#projective-geometry-demo-hide').html($('#projective-geometry-demo-input'));
     let $input = $('#perspective-projection-demo-input');
-    $('#perspective-projection-demo-start').html($inpu.html());
+    $('#perspective-projection-demo-start').html($input);
 }
 
 persObliqueButton.onclick = function() {
     PERS.oblique();
     let $input = $('#perspective-projection-demo-input');
-    $('#perspective-projection-demo-oblique').html($input.html());
+    $('#perspective-projection-demo-oblique').html($input);
 }
 
